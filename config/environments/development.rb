2ba30579e0ab8987afa3d63e1bcd2ae418fcb509
Pascal Pencil <pascal@superpencil.com>
2016-06-13 10:17:14 +0900

    React

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,4 +48,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # React
+  config.react.variant = :development
 end
