2290419b252fea028db7badcc522fbd3587a4d58
Pascal Pencil <pascal@superpencil.com>
2016-08-22 15:37:51 +0900

    Update to Devise 4>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,8 +34,8 @@ before_filter :configure_permitted_parameters, if: :devise_controller?
 protected
 
   def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password) }
-      devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :current_password) }
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email, :password, :current_password])
   end
 
 end
