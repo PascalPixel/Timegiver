c96dc1de37c513b57f57c3f041d653e8da133762
Pascal Pencil <pascal@superpencil.com>
2016-08-27 11:27:16 +0900

    I18n

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
+
   # Scope manages languages
-  scope "(:locale)", locale: /#{I18n.available_locales.join('|')}/ do
+  scope "/:locale", locale: /#{I18n.available_locales.join('|')}/ do
 
     # Devise
     devise_for :users
-
-    # Public
-    # get "about", to: "welcomes#about"
 
     # Private
     authenticate :user do
@@ -19,9 +17,10 @@ Rails.application.routes.draw do
 
     # Reroute bad links to home
     get "*path", to: "welcomes#index"
+
   end
 
   # Redirect to language
-  root to: redirect("#{I18n.default_locale}", status: 302), as: :redirected_root
-  get "*path", to: redirect("#{I18n.default_locale}/%{path}", status: 302), constraints: {path: /(?!(#{I18n.available_locales.join('|')})\/).*/}, format: false
+  root to: redirect("/#{I18n.default_locale}", status: 302), as: :redirected_root
+  get "/*path", to: redirect("/#{I18n.default_locale}/%{path}", status: 302), constraints: {path: /(?!(#{I18n.available_locales.join('|')})\/).*/}, format: false
 end
