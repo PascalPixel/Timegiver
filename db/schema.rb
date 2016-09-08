76ecccfbb41e02fc0d85107fff0e39821ac8512a
Pascal Pencil <pascal@superpencil.com>
2016-09-08 14:06:17 +0900

    Fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160906020205) do
+ActiveRecord::Schema.define(version: 20160908044241) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,7 +37,7 @@ ActiveRecord::Schema.define(version: 20160906020205) do
 
   create_table "teams", force: :cascade do |t|
     t.text     "description"
-    t.string   "team_name"
+    t.string   "name"
     t.string   "color"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
